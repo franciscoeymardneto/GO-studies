b4cf0a5776405065dcc9f6d2e86859780255a470
eymard <eymard@sysautomacao.com.br>
2022-12-22 23:40:01 -0300

    Configure the log package to print the command name ("greetings: ") at the start of its log messages, without a time stamp or source file information. Assign both of the Hello return values, including the error, to variables. Change the Hello argument from Gladys’s name to an empty string, so you can try out your error-handling code. Look for a non-nil error value. There's no sense continuing in this case. Use the functions in the standard library's log package to output error information. If you get an error, you use the log package's Fatal function to print the error and stop the program.

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Eymard")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+
+	// Request a greeting message
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 
 }
