2d6f794fb9ffb99cd5082da8b93c75510c701bba
eymard <eymard@sysautomacao.com.br>
2023-01-02 22:36:29 -0300

    log the multiple greetings

--- hello/hello.go
+++ hello/hello.go
@@ -16,12 +16,20 @@ func main() {
 
 	// Request a greeting message
 
-	message, err := greetings.Hello("Eymard")
+	// message, err := greetings.Hello("Eymard")
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 
 }
